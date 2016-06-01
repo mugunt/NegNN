eebd086b0166e090e53314e718b549ab419c82e2
Federico Fancellu <ffancellu@cngl.ie>
2016-06-01 12:51:41 +0100

    Deal with <UNK> int_proc.py

--- bilstm/int_processor.py
+++ bilstm/int_processor.py
@@ -55,6 +55,8 @@ def build_vocab(sents, tags, tags_uni, labels,lengths):
         return dict([(w,i) for i,w in enumerate(cnt.keys())])
 
     w2idxs = token2idx(Counter(chain(*sents)))
+    # add <UNK> token
+    w2idxs['<UNK>'] = max(token2idx.values())+1
     t2idxs = token2idx(Counter(chain(*tags)))
     tuni2idxs= token2idx(Counter(chain(*tags_uni)))
     y2idxs = {'I':0,'O':1,'E':2}
@@ -69,7 +71,8 @@ def build_vocab(sents, tags, tags_uni, labels,lengths):
 
 def build_input_data(voc, sents, tags, tags_uni, cues, scopes, labels):
 
-    words_idxs = [np.array([voc['w2idxs'][w] for w in sent],dtype=np.int32) for sent in sents]
+    words_idxs = [np.array([voc['w2idxs'][w] for w in sent if w in voc['w2idxs'] else voc['w2idxs']["<UNK>"]],dtype=np.int32) for sent in sents]
+    
     tags_idxs = [np.array([voc['t2idxs'][t] for t in tag_sent],dtype=np.int32) for tag_sent in tags]
     tags_uni_idxs = [np.array([voc['tuni2idxs'][tu] for tu in tag_sent_uni],dtype=np.int32) for tag_sent_uni in tags_uni]
     y_idxs = [np.array([voc['y2idxs'][y] for y in y_array],dtype=np.int32) for y_array in labels]
